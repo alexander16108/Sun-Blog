084f63649e91d30db302b104737cc3f401b62561
alexander16108 <alexandermayowa05@gmail.com>
2022-02-23 13:03:58 +0100

    refactor tests

--- spec/models/comment.rb
+++ spec/models/comment.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
+RSpec.describe Comment, type: :model do
   subject do
     user = User.new(name: 'Lenny', photo: 'image', bio: 'bio', posts_counter: 0)
     Post.new(title: 'To Be', text: 'The big question is: "To be or not to be a Ruby programmer"',
              likes_counters: 0, comments_counter: 0, user: user)
   end
 
-  user = User.new(name: 'Lenny', photo: 'image', bio: 'bio', posts_counter: 0)
-
-  before { user.save }
+  before { subject.save }
 
   it 'should return most recent comments' do
-    user.likes.create!(post: subject)
-    user.likes.create!(post: subject)
-    user.likes.create!(post: subject)
+    user = User.new(name: 'Lenny', photo: 'image', bio: 'bio', posts_counter: 0)
+    subject.comments.create!(user: user, text: 'Well said.')
+    subject.comments.create!(user: user, text: 'I would not have said it better')
 
-    expect(subject.likes_counters).to eql 3
+    expect(subject.comments_counter).to eql 2
   end
 end
